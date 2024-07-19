40f590578b46e953cecd926f6c9cc9e4931c2aa0
sorozco <sergiorozco.ochoa@gmail.com>
2024-07-19 14:58:23 -0500

    Adding Sticky Scroll navigation

--- src/App.vue
+++ src/App.vue
@@ -80,12 +80,12 @@ function downloadFile(file) {
     </label>
 </div> 
   </div>
-  <main class="flex px-12 gap-x-2 my-5" v-if="renderedEmail[0]">
+  <main class="flex px-12 gap-x-2 py-5" v-if="renderedEmail[0]">
     <section class="w-1/3 px-2">
       <div class="w-full bg-emerald-300 py-2 px-3 rounded-t-lg">
         <p>Total Emails: {{ totalEmails }} </p>
       </div>
-      <ul class="flex flex-col border border-neutral-200 overflow-y-auto h-full">
+      <ul class="flex flex-col border border-neutral-200 overflow-y-auto">
         <li @click="selectEmail(email, index)"
           :class="selectedEmail === renderedEmail[index] ? '!bg-emerald-700 text-white hover:!bg-emerald-600' : 'bg-neutral-50 hover:bg-neutral-200'"
           class="flex gap-x-4 py-4 px-2 items-start text-sm cursor-pointer border-b last:border-b-0 border-b-neutral-200"
