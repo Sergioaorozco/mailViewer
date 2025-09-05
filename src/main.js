c84b8ee80cb9a07a00bd869ea49dd693dd2edf95
sorozco <sergiorozco.ochoa@gmail.com>
2025-09-05 17:49:07 -0500

    Upgrading js libraries to improve performance

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import Aura from '@primeuix/themes/aura';
 import FileUpload from 'primevue/fileupload';
 
 const app = createApp(App);
 app.component('FileUpload',FileUpload);
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  theme: {
+    Aura
+  }
+});
 app.mount('#app');
