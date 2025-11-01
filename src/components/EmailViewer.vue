c7aff625bd5ffde1b6b63b1f28385e310c9ca60d
sorozco <sergiorozco.ochoa@gmail.com>
2025-10-31 21:50:08 -0500

    Rendering emails

--- src/components/EmailViewer.vue
+++ src/components/EmailViewer.vue
@@ -43,20 +43,20 @@ const donwloadAttachment = ( attachment) => {
     <div class="flex">
       <article class="w-1/3">
         <input type="text" placeholder="Search emails..." class="mb-4 p-2 w-full rounded" />
-        <ul class="rounded-lg overflow-clip border border-zinc-300 dark:border-zinc-800">
+        <ul class="rounded-lg overflow-clip border border-zinc-300">
           <li class="bg-lime-400 px-4 py-4 text-zinc-800 font-semibold">Emails</li>
           <li
-            v-for="email in parsedEmails"
+            v-for="email in emails"
             :key="email.subject + email.date"
             @click="selectedEmail = email"
-            class="bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-900 dark:hover:bg-zinc-800 px-3 py-2 border-b border-zinc-300 dark:border-b-zinc-800 last:border-none"
+            class="bg-zinc-100 hover:bg-zinc-300 px-3 py-2 border-b border-zinc-300 last:border-none"
           >
             {{ email.subject }}
           </li>
         </ul>
       </article>
       <article class="p-5 w-2/3 rounded-lg overflow-clip">
-        <div class="border border-zinc-700 rounded-lg p-4 mb-4 bg-zinc-900" v-if="selectedEmail.subject">
+        <div class="border border-zinc-200 rounded-lg p-4 mb-4 bg-zinc-100" v-if="selectedEmail.subject">
           <p>{{ selectedEmail.subject }}</p>
           <p>From: {{ selectedEmail.from }}</p>
           <p>To: {{ selectedEmail.to }}</p>
@@ -64,7 +64,7 @@ const donwloadAttachment = ( attachment) => {
           <ul>
             <li>Attachments:</li>
             <li class="flex gap-x-3" v-for="file in selectedEmail.attachments">
-              <button class="flex gap-x-3 px-3 py-2 rounded-md bg-zinc-600" @click="donwloadAttachment">
+              <button class="flex gap-x-3 px-3 py-2 rounded-md bg-zinc-100" @click="donwloadAttachment">
                 <i><PaperClipIcon /></i>
                 {{file.filename}}
               </button>
